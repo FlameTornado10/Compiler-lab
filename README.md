3e46c044f445e97bfe5cb9cc3890f560b5cd5565
FlameTornado10 <7blackbottle@gmail.com>
2022-07-13 22:26:09 +0800

    Compiler lab code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Compiler-lab
+
+UCAS 编译原理实验课程代码，主要编写部分在compiler_dzk/compiler/src/中
+
+* 前端基于Antlr4实现
+
+* 中间代码为三元表达式
+
+* 生成Risc-V汇编
+
+  * 进行了简单的寄存器分配
+    * todo: 线性扫描
+
+  
---
+++ compiler_dzk/prac/prac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int func(float a){
+    {
+        int a = 0;
+        return a;
+    }
+}
+int main(){
+    // printf("%d",func(0));
+    int x;
+    x = 4*+5;
+    printf("%d\n",x);
+}
